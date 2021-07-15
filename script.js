8fab7cd7b2aa8415650b1b72044ff94e60d497ec
Jaap Brasser <noreply@jaapbrasser.com>
2021-07-15 14:52:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ if (captcha_label) {
 document.write(captcha_html);
 
 window.addEventListener('message', function(e) {
-    if (e.origin.indexOf('ddog.nl') == 1) {
+    if (e.origin.indexOf('ddog.nl') > -1) {
         captcha_done = true;
         document.getElementById('doom_captcha').closest('form').action = 'https://getform.io/f/';
         document.getElementById('doom_captcha').style.borderColor = 'black';
